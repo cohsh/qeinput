e857adfa2bf39700fa1b84f22a28ba92ad7ebdab
cohsh <82075327+cohsh@users.noreply.github.com>
2023-02-01 13:42:51 +0900

    [feat] formats: for nscf

--- qeinput/formats/pw.py
+++ qeinput/formats/pw.py
@@ -19,12 +19,40 @@ ecutrho = {ecutrho}
 occupations = '{occupations}'
 /
 &electrons
-conv_thr        = {conv_thr}
+conv_thr = {conv_thr}
 /
 ATOMIC_SPECIES
 {atomic_species}
 ATOMIC_POSITIONS alat
 {atomic_positions}
 K_POINTS automatic
-{k_points}
+{k_points_automatic}
+''')
+
+pw["nscf"] = textwrap.dedent('''
+&control
+prefix = '{prefix}'
+calculation = 'nscf'
+pseudo_dir = '{pseudo_dir}'
+outdir = './'
+/
+&system
+ibrav = {ibrav}
+{abc}
+nat = {nat}
+ntyp = {ntyp}
+ecutwfc = {ecutwfc}
+ecutrho = {ecutrho}
+occupations = '{occupations}'
+nbnd = {nbnd}
+/
+&electrons
+conv_thr = {conv_thr}
+/
+ATOMIC_SPECIES
+{atomic_species}
+ATOMIC_POSITIONS alat
+{atomic_positions}
+K_POINTS crystal
+{k_points_crystal}
 ''')
